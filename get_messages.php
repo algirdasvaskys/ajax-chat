da830868fa61e74cc8ee61cf9e2842f8701ea42c
algirdasvaskys <168381335+algirdasvaskys@users.noreply.github.com>
2025-06-21 17:44:20 +0100

    get_messages.php

--- get_messages.php
+++ get_messages.php
@@ -1,1 +1,8 @@
+<?php
+require 'db_connection.php';
 
+$stmt = $pdo->query("SELECT username, message FROM messages ORDER BY id DESC LIMIT 50");
+$messages = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode(array_reverse($messages));
+?>
