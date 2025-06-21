c678528d4774d0bcb51f03574069d0f27b04f9ba
algirdasvaskys <168381335+algirdasvaskys@users.noreply.github.com>
2025-06-21 17:43:51 +0100

    post_message.php

--- post_message.php
+++ post_message.php
@@ -1,1 +1,11 @@
+<?php
+require 'db_connection.php';
 
+$username = $_POST['username'] ?? '';
+$message = $_POST['message'] ?? '';
+
+if (!empty($username) && !empty($message)) {
+    $stmt = $pdo->prepare("INSERT INTO messages (username, message) VALUES (?, ?)");
+    $stmt->execute([$username, $message]);
+}
+?>
